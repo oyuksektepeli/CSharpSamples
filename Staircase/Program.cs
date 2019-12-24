780e7b1dac73100cf754b3cde507faec230b6790
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-12-25 01:18:39 +0300

    Staircase added

---
+++ Staircase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void staircase(int n)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
