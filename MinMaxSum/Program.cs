4790b03e2a17cdcbb9a5ccb37c3e45c9c44e5e98
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-12-28 22:03:37 +0300

    maxmin sum added

---
+++ MinMaxSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace MinMaxSum
+{    
+    class Program
+    {
+        static void miniMaxSum(int[] arr)
+        {
+            long max = arr.Max();
+            long min = arr.Min();
+            long sum = 0;
+
+            foreach (var item in arr)
+            {
+                sum += item ;
+            }
+            Console.WriteLine($"{sum - max} {sum - min}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
