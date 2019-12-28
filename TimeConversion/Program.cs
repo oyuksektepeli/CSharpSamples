ea9ce59b7090d3759a2cae0e5716fdaf4fa4ae03
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-12-29 01:48:21 +0300

    Time Conversion added.

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static string timeConversion(string s)
+            {
+                DateTime timeValue = Convert.ToDateTime(s);
+                string result = timeValue.ToString("HH:mm:ss");
+                return result;
+
+            }
+
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+
+            Console.WriteLine(result);
+
+            
+        }
+    }
+}
