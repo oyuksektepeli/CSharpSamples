0fb371f70caa5fa63c595c9ace9b34235febf7f7
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-12-30 00:28:00 +0300

    binary search, leaderboard added, combine two array

---
+++ ClimbingtheLeaderboard/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace ClimbingtheLeaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int[] climbingLeaderboard(int[] scores, int[] alice)
+            {
+                int[] aliceranks = new int[alice.Length];
+
+                int[] singularscores = scores.Distinct().ToArray();
+                
+                
+                for (int i = 0; i < alice.Length; i++)
+                {
+                    for (int j = 0; j < singularscores.Length; j++)
+                    {
+
+                        if (alice[i] > singularscores[j])
+                        {
+                            aliceranks[i] = j + 1;
+                            break;
+
+                        }
+                        else if (alice[i] == singularscores[j])
+                        {
+                            aliceranks[i] = j + 1;
+                            break;
+                        }
+                        else
+                        {
+                            aliceranks[i] = j + 2;
+
+                        }
+                    }
+                }
+                                
+                return aliceranks;
+
+
+
+            }
+
+
+            int scoresCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp))
+            ;
+            int aliceCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] alice = Array.ConvertAll(Console.ReadLine().Split(' '), aliceTemp => Convert.ToInt32(aliceTemp))            ;
+            int[] result = climbingLeaderboard(scores, alice);
+
+            Console.WriteLine(string.Join("\n", result));
+
+            
+        }
+    }
+}
---
+++ CombineTwoArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CombineTwoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = { 4, 5, 6 };
+
+            var combined = new List<int>();
+            combined.AddRange(arr1);
+            combined.AddRange(arr2);
+            Array.Sort(combined.ToArray());
+
+            foreach (var item in combined)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
